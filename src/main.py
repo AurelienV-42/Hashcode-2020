c6ec84051859f8b7315238c21dbf6f48e76e1fbc
Aurélien Vandaële <aurelien.vandaele@epitech.eu>
2020-02-20 19:24:38 +0100

    [+] Parsing and global variables

--- src/main.py
+++ src/main.py
@@ -1,10 +1,32 @@
 #!/usr/bin/env python3
 
+import sys
+
+B = 0
+L = 0
+D = 0
+
+
+def parse(path_to_file):
+    global B, L, D
+    file = open(path_to_file, "r")
+    content = file.read()
+    i = 0
+    splitted = content.split('\n')
+    for line in splitted:
+        numbers = line.split(' ')
+        if i == 0:
+            B = int(numbers[0])
+            L = int(numbers[1])
+            D = int(numbers[2])
+        i += 1
+    return content
+
 
 def main():
-    print("Stiix il est nul")
+    print(parse(sys.argv[1]))
+    print(B, L, D)
 
 
 if __name__ == "__main__":
-    # execute only if run as a script
     main()
