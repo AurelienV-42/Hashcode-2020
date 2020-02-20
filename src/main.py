35f0a0385470a01711159e379bd67443597731be
Aurélien Vandaële <aurelien.vandaele@epitech.eu>
2020-02-20 19:49:04 +0100

    [+] Parsing with all libraries

--- src/main.py
+++ src/main.py
@@ -5,27 +5,45 @@ import sys
 B = 0
 L = 0
 D = 0
+S = []
+Library = []
 
 
 def parse(path_to_file):
-    global B, L, D
+    global B, L, D, S, Library
     file = open(path_to_file, "r")
     content = file.read()
     i = 0
     splitted = content.split('\n')
+    new_library = {"N": 0, "T": 0, "M": 0, "IDS": []}
     for line in splitted:
         numbers = line.split(' ')
-        if i == 0:
+        if len(numbers) == 1:
+            return content
+        elif i == 0:
             B = int(numbers[0])
             L = int(numbers[1])
             D = int(numbers[2])
+        elif i == 1:
+            for tmp in numbers:
+                S += tmp
+        elif i % 2 == 0:
+            new_library["N"] = int(numbers[0])
+            new_library["T"] = int(numbers[1])
+            new_library["M"] = int(numbers[2])
+            new_library["IDS"] = []
+        else:
+            for tmp in numbers:
+                new_library["IDS"] += tmp
+            Library += new_library.copy(),
         i += 1
     return content
 
 
 def main():
-    print(parse(sys.argv[1]))
     print(B, L, D)
+    print(S)
+    print(Library)
 
 
 if __name__ == "__main__":
